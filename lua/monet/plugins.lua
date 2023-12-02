e41bb3346a9a5618e57b407d14ea2759290fc884
fynnfluegge <fynnfluegge@gmx.de>
2023-12-02 12:00:46 +0100

    add more plugins

--- lua/monet/plugins.lua
+++ lua/monet/plugins.lua
@@ -40,6 +40,67 @@ function M.get()
 		AerialNullIcon = { link = "@type" },
 
 		-- Alpha
+		AlphaShortcut = { fg = C.mossgreen },
+		AlphaHeader = { fg = C.softblue },
+		AlphaHeaderLabel = { fg = C.orange },
+		AlphaButtons = { fg = C.pastelturqoise },
+		AlphaFooter = { fg = C.yellow, style = { "italic" } },
+
+		-- Barbar
+		BufferCurrent = { bg = C.darkgrey1, fg = C.grey0 },
+		BufferCurrentIndex = { bg = C.darkgrey1, fg = C.softblue },
+		BufferCurrentMod = { bg = C.darkgrey1, fg = C.yellow },
+		BufferCurrentSign = { bg = C.darkgrey1, fg = C.softblue },
+		BufferCurrentTarget = { bg = C.darkgrey1, fg = C.red },
+		BufferVisible = { bg = C.dark1, fg = C.grey0 },
+		BufferVisibleIndex = { bg = C.dark1, fg = C.softblue },
+		BufferVisibleMod = { bg = C.dark1, fg = C.yellow },
+		BufferVisibleSign = { bg = C.dark1, fg = C.softblue },
+		BufferVisibleTarget = { bg = C.dark1, fg = C.red },
+		BufferInactive = { bg = C.dark1, fg = C.grey5 },
+		BufferInactiveIndex = { bg = C.dark1, fg = C.grey5 },
+		BufferInactiveMod = { bg = C.dark1, fg = C.yellow },
+		BufferInactiveSign = { bg = C.dark1, fg = C.softblue },
+		BufferInactiveTarget = { bg = C.dark1, fg = C.red },
+		BufferTabpages = { bg = C.dark1, fg = C.none },
+		BufferTabpage = { bg = C.dark1, fg = C.softblue },
+
+		-- Cmp
+		CmpItemAbbr = { fg = C.grey3 },
+		CmpItemAbbrDeprecated = { fg = C.grey5, style = { "strikethrough" } },
+		CmpItemKind = { fg = C.softblue },
+		CmpItemMenu = { fg = C.grey0 },
+		CmpItemAbbrMatch = { fg = C.grey0, style = { "bold" } },
+		CmpItemAbbrMatchFuzzy = { fg = C.grey0, style = { "bold" } },
+
+		CmpItemKindSnippet = { fg = C.cornflowerblue },
+		CmpItemKindKeyword = { fg = C.red },
+		CmpItemKindText = { fg = C.softaqua },
+		CmpItemKindMethod = { fg = C.softblue },
+		CmpItemKindConstructor = { fg = C.softblue },
+		CmpItemKindFunction = { fg = C.softblue },
+		CmpItemKindFolder = { fg = C.softblue },
+		CmpItemKindModule = { fg = C.softblue },
+		CmpItemKindConstant = { fg = C.orange },
+		CmpItemKindField = { fg = C.mossgreen },
+		CmpItemKindProperty = { fg = C.mossgreen },
+		CmpItemKindEnum = { fg = C.mossgreen },
+		CmpItemKindUnit = { fg = C.mossgreen },
+		CmpItemKindClass = { fg = C.yellow },
+		CmpItemKindVariable = { fg = C.pastelpink },
+		CmpItemKindFile = { fg = C.softblue },
+		CmpItemKindInterface = { fg = C.yellow },
+		CmpItemKindColor = { fg = C.red },
+		CmpItemKindReference = { fg = C.red },
+		CmpItemKindEnumMember = { fg = C.red },
+		CmpItemKindStruct = { fg = C.softblue },
+		CmpItemKindValue = { fg = C.orange },
+		CmpItemKindEvent = { fg = C.softblue },
+		CmpItemKindOperator = { fg = C.softblue },
+		CmpItemKindTypeParameter = { fg = C.softblue },
+		CmpItemKindCopilot = { fg = C.softaqua },
+
+		-- Dashboard
 		DashboardShortCut = { fg = C.skyblue },
 		DashboardHeader = { fg = C.softblue },
 		DashboardCenter = { fg = C.mossgreen },
@@ -50,6 +111,109 @@ function M.get()
 		DashboardKey = { fg = C.orange },
 		DashboardDesc = { fg = C.softblue },
 		DashboardIcon = { fg = C.skyblue, bold = true },
+
+		-- Flash
+		FlashBackdrop = { fg = C.grey5 },
+		FlashLabel = { fg = C.springgreen, bg = active_bg, style = { "bold" } },
+		FlashMatch = { fg = C.pastelturqoise, bg = active_bg },
+		FlashCurrent = { fg = C.orange, bg = active_bg },
+		FlashPrompt = { link = "NormalFloat" },
+
+		-- Mason
+		MasonHeader = {
+			fg = O.transparent_background and C.pastelturqoise or C.dark0,
+			bg = O.transparent_background and C.none or C.pastelturqoise,
+			style = { "bold" },
+		},
+		MasonHeaderSecondary = {
+			fg = O.transparent_background and C.softblue or C.dark0,
+			bg = O.transparent_background and C.none or C.softblue,
+			style = { "bold" },
+		},
+
+		MasonHighlight = { fg = C.mossgreen },
+		MasonHighlightBlock = {
+			bg = O.transparent_background and C.none or C.mossgreen,
+			fg = O.transparent_background and C.mossgreen or C.dark0,
+		},
+		MasonHighlightBlockBold = {
+			fg = O.transparent_background and C.softblue or C.dark0,
+			bg = O.transparent_background and C.none or C.softblue,
+			bold = true,
+		},
+
+		MasonHighlightSecondary = { fg = C.cornflowerblue },
+		MasonHighlightBlockSecondary = {
+			fg = O.transparent_background and C.softblue or C.dark0,
+			bg = O.transparent_background and C.none or C.softblue,
+		},
+		MasonHighlightBlockBoldSecondary = {
+			fg = O.transparent_background and C.pastelturqoise or C.dark0,
+			bg = O.transparent_background and C.none or C.pastelturqoise,
+			bold = true,
+		},
+
+		MasonMuted = { fg = C.grey5 },
+		MasonMutedBlock = {
+			bg = O.transparent_background and C.none or C.grey5,
+			fg = O.transparent_background and C.grey5 or C.dark0,
+		},
+		MasonMutedBlockBold = { bg = C.yellow, fg = C.dark0, bold = true },
+
+		MasonError = { fg = C.red },
+
+		MasonHeading = { fg = C.pastelturqoise, bold = true },
+
+		-- Mini
+		MiniCompletionActiveParameter = { style = { "underline" } },
+
+		MiniCursorword = { style = { "underline" } },
+		MiniCursorwordCurrent = { style = { "underline" } },
+
+		MiniIndentscopeSymbol = { fg = C.grey0 },
+		MiniIndentscopePrefix = { style = { "nocombine" } }, -- Make it invisible
+
+		MiniJump = { fg = C.grey3, bg = C.skyblue },
+
+		MiniJump2dSpot = { bg = C.dark0, fg = C.orange, style = { "bold", "underline" } },
+
+		MiniStarterCurrent = {},
+		MiniStarterFooter = { fg = C.yellow, style = { "italic" } },
+		MiniStarterHeader = { fg = C.softblue },
+		MiniStarterInactive = { fg = C.darkgrey0, style = O.styles.comments },
+		MiniStarterItem = { fg = C.grey0 },
+		MiniStarterItemBullet = { fg = C.softblue },
+		MiniStarterItemPrefix = { fg = C.skyblue },
+		MiniStarterSection = { fg = C.pastelpink },
+		MiniStarterQuery = { fg = C.mossgreen },
+
+		MiniStatuslineDevinfo = { fg = C.grey1, bg = C.darkgrey1 },
+		MiniStatuslineFileinfo = { fg = C.grey1, bg = C.darkgrey1 },
+		MiniStatuslineFilename = { fg = C.grey0, bg = C.mantle },
+		MiniStatuslineInactive = { fg = C.softblue, bg = C.mantle },
+		MiniStatuslineModeCommand = { fg = C.dark0, bg = C.orange, style = { "bold" } },
+		MiniStatuslineModeInsert = { fg = C.dark0, bg = C.mossgreen, style = { "bold" } },
+		MiniStatuslineModeNormal = { fg = C.mantle, bg = C.softblue, style = { "bold" } },
+		MiniStatuslineModeOther = { fg = C.dark0, bg = C.softaqua, style = { "bold" } },
+		MiniStatuslineModeReplace = { fg = C.dark0, bg = C.red, style = { "bold" } },
+		MiniStatuslineModeVisual = { fg = C.dark0, bg = C.cornflowerblue, style = { "bold" } },
+
+		MiniSurround = { bg = C.skyblue, fg = C.darkgrey1 },
+
+		MiniTablineCurrent = { fg = C.grey0, bg = C.dark0, sp = C.red, style = { "bold", "italic", "underline" } },
+		MiniTablineFill = { bg = active_bg },
+		MiniTablineHidden = { fg = C.grey0, bg = inactive_bg },
+		MiniTablineModifiedCurrent = { fg = C.red, bg = C.none, style = { "bold", "italic" } },
+		MiniTablineModifiedHidden = { fg = C.red, bg = C.none },
+		MiniTablineModifiedVisible = { fg = C.red, bg = C.none },
+		MiniTablineTabpagesection = { fg = C.darkgrey1, bg = C.dark0 },
+		MiniTablineVisible = { bg = C.none },
+
+		MiniTestEmphasis = { style = { "bold" } },
+		MiniTestFail = { fg = C.red, style = { "bold" } },
+		MiniTestPass = { fg = C.mossgreen, style = { "bold" } },
+
+		MiniTrailspace = { bg = C.red },
 
 		-- Neo-tree
 		NeoTreeDirectoryName = { fg = C.softblue },
@@ -67,7 +231,7 @@ function M.get()
 		NeoTreeGitIgnored = { fg = C.grey5 },
 		NeoTreeGitModified = { fg = C.yellow },
 		NeoTreeGitUnstaged = { fg = C.red },
-		NeoTreeGitUntracked = { fg = C.cornflowerblue },
+		NeoTreeGitUntracked = { fg = C.skyblue },
 		NeoTreeGitStaged = { fg = C.mossgreen },
 
 		NeoTreeFloatBorder = { link = "FloatBorder" },
@@ -82,6 +246,32 @@ function M.get()
 		NeoTreeTabSeparatorInactive = { fg = inactive_bg, bg = inactive_bg },
 		NeoTreeVertSplit = { fg = C.dark0, bg = inactive_bg },
 		NeoTreeStatusLineNC = { fg = C.dark1, bg = C.dark1 },
+
+		-- Noice
+		NoiceCmdline = { fg = C.grey0 },
+		NoiceCmdlineIcon = { fg = C.lightcyan },
+		NoiceCmdlineIconSearch = { fg = C.yellow },
+		NoiceCmdlinePopupBorder = { fg = C.pastelturqoise },
+		NoiceCmdlinePopupBorderSearch = { fg = C.yellow },
+		NoiceConfirmBorder = { fg = C.softblue },
+		NoiceMini = { fg = C.grey0, blend = 0 },
+
+		-- Notify
+		NotifyERRORBorder = { fg = C.red },
+		NotifyERRORIcon = { fg = C.red },
+		NotifyERRORTitle = { fg = C.red, style = { "italic" } },
+		NotifyWARNBorder = { fg = C.yellow },
+		NotifyWARNIcon = { fg = C.yellow },
+		NotifyWARNTitle = { fg = C.yellow, style = { "italic" } },
+		NotifyINFOBorder = { fg = C.softblue },
+		NotifyINFOIcon = { fg = C.softblue },
+		NotifyINFOTitle = { fg = C.softblue, style = { "italic" } },
+		NotifyDEBUGBorder = { fg = C.orange },
+		NotifyDEBUGIcon = { fg = C.orange },
+		NotifyDEBUGTitle = { fg = C.orange, style = { "italic" } },
+		NotifyTRACEBorder = { fg = C.pastelpeach },
+		NotifyTRACEIcon = { fg = C.pastelpeach },
+		NotifyTRACETitle = { fg = C.pastelpeach, style = { "italic" } },
 
 		-- Rainbow Delimiters
 		RainbowDelimiterRed = { fg = C.pastelpink },
@@ -119,6 +309,10 @@ function M.get()
 		TroubleText = { fg = C.mossgreen },
 		TroubleCount = { fg = C.skyblue, bg = O.transparent_background and C.none or C.darkgrey1 },
 		TroubleNormal = { fg = C.grey0, bg = O.transparent_background and C.none or C.darkgrey0 },
+
+		-- Ufo
+		UfoFoldedFg = { fg = C.pastelturqoise },
+		UfoFoldedEllipsis = { fg = C.darkgrey0, bg = C.softblue },
 	}
 end
 
